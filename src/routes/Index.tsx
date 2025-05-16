8006811eadcff2221bbe1f6dc0ecb35f65576d5f
Kieran Davis <davisk4rpi@gmail.com>
2025-05-16 15:48:15 -0700

    updates under construction page

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -3,14 +3,16 @@ import { config } from "../../config";
 export function Index() {
   return (
     <div className="w-screen h-screen flex flex-col items-center bg-black">
-      <img
-        src={`${config.imageCdnUrl}/PXL_20230904_215813241.jpg?f=webp`}
-        alt="Atypical Village"
-        className="w-full h-full object-contain md:object-cover absolute"
-      />
-      <h1 className="text-white text-6xl sm:text-8xl md:text-9xl font-bold z-10 text-shadow-gray-900 text-shadow-lg mt-10">
-        TYPICAL
+      <h1 className="inline flex-grow-0 text-white text-[2.5rem] sm:text-7xl md:text-8xl font-bold z-10 text-shadow-gray-900 text-shadow-lg my-10">
+        UNDER
+        <br />
+        CONSTRUCTION
       </h1>
+      <img
+        src={`${config.imageCdnUrl}/2023-typical-sign-needs-work.jpg?f=webp`}
+        alt="Atypical Village"
+        className="flex-1 object-contain md:object-cover object-top"
+      />
     </div>
   );
 }

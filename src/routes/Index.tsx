877955774a3c12cec714b92e252be01b8f3e63b7
Kieran Davis <davisk4rpi@gmail.com>
2025-05-16 10:21:24 -0700

    updates manifest

--- src/routes/Index.tsx
+++ src/routes/Index.tsx
@@ -8,7 +8,7 @@ export function Index() {
         alt="Atypical Village"
         className="w-full h-full object-contain md:object-cover absolute"
       />
-      <h1 className="text-white text-6xl lg:text-9xl font-bold z-10 text-shadow-gray-900 text-shadow-lg mt-5">
+      <h1 className="text-white text-6xl sm:text-8xl md:text-9xl font-bold z-10 text-shadow-gray-900 text-shadow-lg mt-10">
         TYPICAL
       </h1>
     </div>
